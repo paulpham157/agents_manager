375210701b48acc4abb1c387a54406690e1a159a
sandeshnaroju <sandeshnaroju@gmail.com>
2025-02-26 00:05:11 +0530

    Added llama support

--- agents_manager/models/Grok.py
+++ agents_manager/models/Grok.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional
+from typing import Any
 
 from openai import OpenAI
 
@@ -8,10 +8,10 @@ from agents_manager.models import OpenAi
 class Grok(OpenAi):
     def __init__(self, name: str, **kwargs: Any) -> None:
         """
-        Initialize the OpenAi model with a name and optional keyword arguments.
+        Initialize the Grok model with a name and optional keyword arguments.
 
         Args:
-            name (str): The name of the OpenAI model (e.g., "gpt-3.5-turbo").
+            name (str): The name of the Grok model (e.g., "grok-2-latest").
             **kwargs (Any): Additional arguments, including optional "api_key".
         """
         super().__init__(name, **kwargs)
@@ -20,6 +20,6 @@ class Grok(OpenAi):
             raise ValueError("A valid  Grok model name is required")
 
         self.client = OpenAI(
-            api_key=kwargs.get("api_key"),  # type: Optional[str]
+            api_key=kwargs.get("api_key"),
             base_url="https://api.x.ai/v1"
         )
